c299d2e01ce2882980882380481b31758613771f
C. Leow <chienleow1@gmail.com>
2020-03-25 16:38:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, student_grade)
+    @roster[student_grade] = []
+    @roster[student_grade] << student_name
+  end
+  
+  def grade(student_grade)
+  
+  end
+  
+end
